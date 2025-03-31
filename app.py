466ff52f99443b41a63943c2ec004e194be57c17
jihoon1212 <rwlgns1013@gmail.com>
2025-03-31 11:21:04 +0900

    'dashboard_update'

--- app.py
+++ app.py
@@ -20,7 +20,52 @@ def invest() :
 # y_data (y축) type = list
 @app.route('/dashboard')
 def dashboard() :
-    return render_template('/dashboard.html')
+    # 유저가 보낸 데이터를 변수에 저장
+    # get 방시긍로 보내온 데이터는 request.args에 존재 
+    # 종목코드
+    input_code =  request.args['code']
+    # 시작 시간
+    input_year = request.args['year']
+    input_month = request.args['month']
+    input_day = request.args['day']
+    input_time = f"{input_year}-{input_month}-{input_day}"
+    # 투자 방식
+    input_type = request.args['type']
+    # 데이터 로드
+    df = invest.load_data(input_code, input_time)
+    # class 생성
+    invest_class = invest.Invest(df,
+                                _col = 'Close',
+                                _start = _input_time)
+    # input_type을 기준으로 class의 함수를 호출
+    if input_type =='bnh':
+        result = invest_class.buyandhold()
+    elif input_type == 'boll':
+        result = invest_class.bollinger()
+    elif input_type == 'mmt':
+        result = invest_class.momentum()
+    # 인덱스 초기화
+    result.reset_index(inplace = True)
+    # 특정 컬럼만 필터
+    result = result[['Date', 'Close', 'trade', 'rtn','acc_rtn']]
+    # 컬럼의 이름을 변경
+    result.colimns = ['시간','종가','보유내역','일별 수익률','누적 수익률']
+    # 컬럼들의 이름을 리스트로 생성
+    cols_list = list(result.columns)
+    # 테이블에 데이터
+    dict_data = result.to_dict(oreint = 'records')
+    # x축 데이터
+    x_data = list(result['시간'])
+    # y축 데이터
+    y_data = list(result[' 일별 수익률'])
+    y1_data = list(result['누적 수익률'])
+    return render_template('/dashboard.html',
+                        table_cols = cols_list,
+                        table_data = dict_data,
+                        x_data = x_data,
+                        y_data = y_data,
+                        y1_data = y1_data
+                        )
 
 
 
